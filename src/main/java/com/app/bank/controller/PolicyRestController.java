32d4693f064fdc78dd343710e7e5df0f1ca10b22
ChaitanyaK5467 <145393847+ChaitanyaK5467@users.noreply.github.com>
2023-10-28 17:33:40 +0530

    Add files via upload

---
+++ src/main/java/com/app/bank/controller/PolicyRestController.java
@@ -0,0 +1,43 @@
+package com.app.bank.controller;
+
+import java.util.List;	
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.bank.entity.Policy;
+import com.app.bank.service.IPolicyService;
+
+@RestController
+@RequestMapping("/policy")
+public class PolicyRestController {
+	
+	@Autowired
+	IPolicyService policyService;
+	
+	@PostMapping("/save")
+	public long savePolicy(@RequestBody Policy policy) {
+		long newPolicyNumber = policyService.savePolicy(policy);
+		return newPolicyNumber;
+	}
+	
+	@GetMapping("/{policyNumber}")
+	public Policy getPolicyByPolicyNumber(@PathVariable long policyNumber) {
+		return policyService.getPolicyByPolicyNumber(policyNumber);
+	}
+	
+	@GetMapping("/allpolicies")
+	public ResponseEntity<List<Policy>> getAllAccounts(){
+		List<Policy> tempList = policyService.getAllPolicies();
+		System.out.println(tempList);
+		return new ResponseEntity<List<Policy>>(tempList, HttpStatus.OK);
+	}
+
+}
